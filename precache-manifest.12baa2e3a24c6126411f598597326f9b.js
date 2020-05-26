b94e988981b6a2e0d171ee2437467e1eefb93ce8
Raniel Brown <BrownRA@advantagegeneral.com>
2020-05-26 09:00:04 -0500

    initial commit

---
+++ precache-manifest.12baa2e3a24c6126411f598597326f9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f865341686d7d594ba3d",
+    "url": "/static/css/main.b634da00.chunk.css"
+  },
+  {
+    "revision": "f865341686d7d594ba3d",
+    "url": "/static/js/main.686cbda8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e869c5603fd19dad7532",
+    "url": "/static/css/2.579de160.chunk.css"
+  },
+  {
+    "revision": "e869c5603fd19dad7532",
+    "url": "/static/js/2.e2054315.chunk.js"
+  },
+  {
+    "revision": "996d7729671ee4aa8bb96ec38b295a4c",
+    "url": "/static/media/bookImage.996d7729.jpg"
+  },
+  {
+    "revision": "2ee5d002c7f2d2117c76c7fa888c3293",
+    "url": "/static/media/hero.2ee5d002.png"
+  },
+  {
+    "revision": "98839c2ffec7441e65c25ebf08db1ed4",
+    "url": "/index.html"
+  }
+];
